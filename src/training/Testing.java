7cbca8dc50c944207641545b86f06cda8fdd7f54
tagrohan <tagrohan@gmail.com>
2021-12-25 23:36:58 +0530

    allIndexes added

--- src/training/Testing.java
+++ src/training/Testing.java
@@ -1,12 +1,31 @@
 package training;
 
+import java.util.Arrays;
+
 public class Testing {
     public static void main(String[] args) {
 
-        System.out.println(findFromLast(new int[]{1, 2, 3, 4, 5, 6, 5, 7}, 5, 0));
+        System.out.println(Arrays.toString(findAllIndexes(new int[]{1, 2, 5, 4, 5, 6, 5, 7}, 5, 0, 0)));
+    }
+
+    // find indexes of all occurrence
+    private static int[] findAllIndexes(int[] arr, int toMatch, int index, int foundSoFar) {
+        if (arr.length == index) {
+            return new int[foundSoFar];
+        }
+        if (arr[index] == toMatch) {
+            foundSoFar += 1;
+        }
+
+        int[] tempIndexes = findAllIndexes(arr, toMatch, index + 1, foundSoFar);
+        if (arr[index] == toMatch) {
+            tempIndexes[foundSoFar - 1] = index;
+        }
+        return tempIndexes;
     }
 
     private static int findFromLast(int[] arr, int toFind, int index) {
+//        System.out.println(findFromLast(new int[]{1, 2, 3, 4, 5, 6, 5, 7}, 5, 0));
         if (arr.length == index) return -1;
 
         int currentIndex = findFromLast(arr, toFind, index + 1);
