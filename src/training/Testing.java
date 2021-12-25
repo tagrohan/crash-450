1d8774ebe4ecb5a8f62fe967fc4ac4c04eb808ab
tagrohan <tagrohan@gmail.com>
2021-12-25 23:24:36 +0530

    fromLast added

--- src/training/Testing.java
+++ src/training/Testing.java
@@ -3,19 +3,30 @@ package training;
 public class Testing {
     public static void main(String[] args) {
 
-        System.out.println(find(new int[]{1, 2, 3, 4, 5, 6, 5, 7}, 5, 0));
+        System.out.println(findFromLast(new int[]{1, 2, 3, 4, 5, 6, 5, 7}, 5, 0));
+    }
+
+    private static int findFromLast(int[] arr, int toFind, int index) {
+        if (arr.length == index) return -1;
+
+        int currentIndex = findFromLast(arr, toFind, index + 1);
+        if (currentIndex != -1) {
+            return currentIndex;
+        } else {
+            return arr[index] == toFind ? index : -1;
+        }
     }
 
     // find from start
-    private static int find(int[] arr, int toFind, int index) {
-
+    private static int findFromStart(int[] arr, int toFind, int index) {
+//        System.out.println(findFromStart(new int[]{1, 2, 3, 4, 5, 6, 5, 7}, 5, 0));
         if (arr.length - 1 == index) {
             return arr[index] == toFind ? index : -1;
         }
         if (arr[index] == toFind) {
             return index;
         }
-        return find(arr, toFind, index + 1);
+        return findFromStart(arr, toFind, index + 1);
 
     }
 }
