ddef7e01f76da5dfa0e890539d22a1e9b0033204
tagrohan <tagrohan@gmail.com>
2022-01-08 14:12:25 +0530

    rev with return

--- src/arrays/Test.java
+++ src/arrays/Test.java
@@ -1,16 +1,35 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
-        System.out.println(sum(1, 4));
+
+        System.out.println(Arrays.toString(rev(new int[]{1,2,2}, 0)));
+    }
+
+
+    private static int[] rev(int[] arr, int index) { // 1 2 3 4 5
+//        System.out.println(Arrays.toString(rev(new int[]{1,2,2}, 0)));
+        if (arr.length / 2 <= index) {               //  2 <= 0
+            return arr;
+        }
+        // swap here
+        int start = arr[index];                 // start = 1
+        arr[index] = arr[arr.length - index - 1];  // arr[0] = arr[4]
+        arr[arr.length - index - 1] = start;      // arr[4] = start
+
+        return rev(arr, index + 1);
+
     }
 
     private static int sum(int start, int end) {
+//        System.out.println(sum(1, 4));
         if (start > end) {
             return 0;
         }
-        int temp = sum(start + 1, end);
-        return temp + start;
+
+        return start + sum(start + 1, end);
     }
 
 
